3f4f849c1937c137c4a80ce494818bbc8f68fa8e
tarunama <sakeharami@gmail.com>
2022-03-17 21:41:54 +0900

    chore(package.json): add lint script for local

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,28 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended"
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": 12,
-		"sourceType": "module"
-	},
-	"plugins": [
-		"@typescript-eslint"
-	],
-	"rules": {
-		"indent": [
-			2,
-			"tab"
-		],
-		"quotes": [
-			2,
-			"double"
-		],
-		"linebreak-style": [
-			2,
-			"unix"
-		],
-		"semi": [
-			2,
-			"always"
-		]
-	}
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["svelte3", "@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  rules: {
+    indent: ["error", 2],
+    quotes: [2, "double"],
+    "linebreak-style": [2, "unix"],
+    semi: [2, "always"],
+  },
+  settings: {
+    "svelte3/typescript": () => require("typescript"), // pass the TypeScript package to the Svelte plugin
+  },
 };
