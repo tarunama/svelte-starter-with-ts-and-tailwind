14327142cc248a2aa84596cb111ed3a4b7d6ade1
tarunama <sakeharami@gmail.com>
2022-03-29 21:24:32 +0900

    wip: add Button Component

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,9 @@ import Tailwindcss from "./Tailwindcss.svelte";
 <Tailwindcss />
 
 <main class="mx-auto my-0 p-8 text-center">
-  <h1 class="mb-4 text-xl font-bold">Svelte + TypeScript + Taliwind CSS</h1>
-  <div>
-	  <p class="mt-1">Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	</div>
+    <h1 class="mb-4 text-xl font-bold">Svelte Material Components</h1>
 	<div class="mt-8 text-center">
-		<h2 class="font-semibold">技術スタック</h2>
+		<h2 class="font-semibold">requires</h2>
 		<div class="text-left w-28 mx-auto my-0">
 			<p>- Svelte</p>
 			<p>- TypeScript</p>
---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let type = "button"
+    export let text = ""
+</script>
+
+<button {type}>
+    {text}
+</button>
