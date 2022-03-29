b9e2ec6cf983123b74224b8e42e37e777c986793
tarunama <sakeharami@gmail.com>
2022-03-29 21:57:54 +0900

    chore: import Button Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-import Tailwindcss from "./Tailwindcss.svelte";
+  import Tailwindcss from "./Tailwindcss.svelte";
+  import Button from "./components/Button.svelte";
 </script>
 
 <Tailwindcss />
 
 <main class="mx-auto my-0 p-8 text-center">
-    <h1 class="mb-4 text-xl font-bold">Svelte Material Components</h1>
-	<div class="mt-8 text-center">
-		<h2 class="font-semibold">requires</h2>
-		<div class="text-left w-28 mx-auto my-0">
-			<p>- Svelte</p>
-			<p>- TypeScript</p>
-			<p>- Tailwind CSS</p>
-		</div>
-	</div>
+  <h1 class="mb-4 text-xl font-bold">Svelte Material Components</h1>
+  <div class="mt-8 text-center">
+    <h2 class="font-semibold">examples</h2>
+    <div class="text-left w-28 mx-auto my-0">
+      <Button text="example button" />
+    </div>
+  </div>
 </main>
