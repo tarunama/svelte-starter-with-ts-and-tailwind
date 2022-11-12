92a71df14b05e33d0d4d73618162c323dd5d53dc
tarunama <sakeharami@gmail.com>
2022-11-12 22:52:54 +0900

    chore: lint

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,9 +3,9 @@
   export let text = "";
   export let classes = "";
 
-  const padding = "px-1 py-2"
+  const padding = "px-1 py-2";
 
-  classes = `${padding}`
+  classes = `${padding}`;
 </script>
 
 <button {type} class={classes}>
